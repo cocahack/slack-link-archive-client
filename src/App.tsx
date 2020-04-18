c705c0306231484862d1857082bf08966c9fcc28
Jake <cocahack@naver.com>
2020-04-18 23:27:01 +0900

    feat: 초기 로딩화면 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import Auth from './auth/auth';
+import BigSpinner from './UI/big-spinner';
 
-function App() {
-  return <Auth />
-}
+const App: FC = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return isLoaded ? (
+    <Switch>
+      <Route path={'/register'} component={Auth} />
+      <Route path={'/enter'} />
+    </Switch>
+  ) : (
+    <BigSpinner />
+  );
+};
 
 export default App;
